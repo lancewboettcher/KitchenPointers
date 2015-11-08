55dd68c7a9888496c9ffffb6c0bb2b18450683f0
Lance Boettcher <lboettch@calpoly.edu>
2015-11-08 13:34:59 -0800

    fixed gitignore

--- kitchenpointers/src/main/java/com/kitchenpointers/HelloController.java
+++ kitchenpointers/src/main/java/com/kitchenpointers/HelloController.java
@@ -2,17 +2,19 @@ package com.kitchenpointers;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @EnableAutoConfiguration
 public class HelloController {
 
-    @RequestMapping("/hello")
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    @ResponseBody
     public String hello() {
-    	System.out.println("heress");
-        return "hello";
+    	System.out.println("hsadasderessss");
+        return "hellos";
     }
 
 }
-
