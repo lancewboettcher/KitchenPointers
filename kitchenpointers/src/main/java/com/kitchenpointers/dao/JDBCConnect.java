e6b4ba506cdd31064a7a1071ae06a40aa8f709df
mackeydailey <mackey_dailey@yahoo.com>
2015-11-10 18:26:51 -0800

    Fixed exception type

--- kitchenpointers/src/main/java/com/kitchenpointers/dao/JDBCConnect.java
+++ kitchenpointers/src/main/java/com/kitchenpointers/dao/JDBCConnect.java
@@ -2,7 +2,6 @@ package com.kitchenpointers.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
@@ -13,7 +12,7 @@ public class JDBCConnect {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
     	  		conn = DriverManager.getConnection("jdbc:mysql://aac3cbjoeutp2a.czmvfbntc6xb.us-west-2.rds.amazonaws.com:3306/kitchenPointers?user=kitchenpointers&password=fkurfess");
-		} catch (SQLException ex) {ex.printStackTrace();}
+		} catch (Exception ex) {ex.printStackTrace();}
 
 		return conn;
 	}
